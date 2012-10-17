ca0f79a62c44bbf733e8545078ee714608f62fd9
Sebastian Krzyszkowiak <dos@dosowisko.net>
2012-10-17 20:11:30 +0200

    more sanity fixes, omgomgomg

---
+++ lists.cpp
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "lists.h"
+
+using namespace std;
+
+List *ListDel(List *LST,List *last){
+	List *TMP;
+	if(last==NULL){
+		TMP=LST;
+		LST=LST->next;
+		free(TMP);
+		TMP=NULL;
+	}
+	else{
+		TMP=LST;
+		last->next=LST->next;
+		LST=last;
+		free(TMP);
+		TMP=NULL;
+	}
+	return LST;
+}
+
+List *ListAdd(List *LST){
+	if (LST==NULL){
+		LST=(List*)malloc(sizeof(List));
+	}
+	else{
+		List *TMP;
+		TMP=LST;
+		while(TMP->next!=NULL){
+			TMP=TMP->next;
+		}
+		TMP->next=(List*)malloc(sizeof(List));
+	}
+	return LST;
+}
--- main.cpp
+++ main.cpp
@@ -315,7 +315,7 @@ void DrawNMB(NPCB *NMB){
 	}
 }
 
-NPCB* MoveNMB(NPCB *NMB){
+List* MoveNMB(NPCB *NMB){
 	NPCB *last;
 	NPCB *TMP;
 	TMP=NMB;
@@ -335,7 +335,7 @@ NPCB* MoveNMB(NPCB *NMB){
 	return TMP;
 }
 
-NPCB* FireN(NPCB *NMB, NPC &NM){
+List* FireN(NPCB *NMB, NPC &NM){
 	if(rand()%20==1){
 		if (NMB==NULL){
 			NMB=(NPCB*)malloc(sizeof(NPCB));
